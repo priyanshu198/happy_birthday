e9e6e22b19955ad4fda5666bef74363382c08064
PRIYANSHU SINGH <57265530+priyanshu198@users.noreply.github.com>
2022-09-30 20:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// var a=prompt("Enter UserName");
+//  var b=prompt("Enter password");
+//   	if(a== "AS" && b=="1913")
+//   	{
+//   	    window.location.href="start.html";
+//   	}
+//   	else
+//   	{
+//   		alert("invalid details");
+//   		window.location.href="index.html";
+//   	}
+  	
